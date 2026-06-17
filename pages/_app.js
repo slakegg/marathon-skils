26943eaa4f9c2d8ad1cb45c6696f0e22462b2f34
slakegg <jlrup40@gmail.com>
2026-06-17 12:54:51 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import { SessionProvider } from 'next-auth/react'
 import Layout from '../components/Layout'
+import ChatWidget from '../components/ChatWidget'
 import '../styles/globals.css'
-
+ 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ChatWidget />
     </SessionProvider>
   )
 }
